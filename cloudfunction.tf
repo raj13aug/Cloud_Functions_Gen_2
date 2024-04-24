ad2fbb6d1cd1c3b8b6af7fe1c31c411c21e91af6
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-04-24 12:13:01 +0530

    first-commit

---
+++ cloudfunction.tf
@@ -0,0 +1,86 @@
+resource "google_project_service" "cf" {
+  project            = var.project_id
+  service            = "cloudfunctions.googleapis.com"
+  disable_on_destroy = true
+}
+
+resource "google_project_service" "cb" {
+  project            = var.project_id
+  service            = "cloudbuild.googleapis.com"
+  disable_on_destroy = true
+}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [google_project_service.cf, google_project_service.cb]
+
+  create_duration = "60s"
+}
+
+# Generates an archive of the source code compressed as a .zip file.
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/tmp/function.zip"
+}
+
+# Add source code zip to the Cloud Function's bucket (Cloud_function_bucket) 
+resource "google_storage_bucket_object" "zip" {
+  source       = data.archive_file.source.output_path
+  content_type = "application/zip"
+  name         = "src-${data.archive_file.source.output_md5}.zip"
+  bucket       = google_storage_bucket.Cloud_function_bucket.name
+  depends_on = [
+    google_storage_bucket.Cloud_function_bucket,
+    data.archive_file.source,
+    time_sleep.wait_30_seconds
+  ]
+}
+
+resource "google_cloudfunctions2_function" "function" {
+  name        = "Cloud-function-trigger-using-terraform-gen-2"
+  location    = var.region
+  description = "Cloud function gen2 trigger using terraform "
+
+  build_config {
+    runtime     = "python39"
+    entry_point = "helloGET"
+    environment_variables = {
+      BUILD_CONFIG_TEST = "build_test"
+    }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.Cloud_function_bucket.name
+        object = google_storage_bucket_object.zip.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 3
+    min_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+    environment_variables = {
+      SERVICE_CONFIG_TEST = "config_test"
+    }
+    ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email          = var.service_account_email
+  }
+
+  event_trigger {
+    trigger_region        = var.region
+    event_type            = "google.cloud.storage.object.v1.finalized"
+    retry_policy          = "RETRY_POLICY_RETRY"
+    service_account_email = var.service_account_email
+    event_filters {
+      attribute = "bucket"
+      value     = google_storage_bucket.input_bucket.name
+    }
+  }
+  depends_on = [
+    google_storage_bucket.Cloud_function_bucket,
+    google_storage_bucket_object.zip,
+    time_sleep.wait_30_seconds
+  ]
+}
---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  description = "Name of a Google Cloud Project"
+  default     = "cloudroot7-demo"
+}
+
+variable "id" {
+  description = "ID of a Google Cloud Project. Can be omitted and will be generated automatically"
+  default     = "mytesting-400910"
+}
+
+variable "project_id" {
+  type        = string
+  description = "project id"
+  default     = "mytesting-400910"
+}
+
+variable "region" {
+  type        = string
+  description = "Region of policy "
+  default     = "us-central1"
+}
+
+variable "service_account_email" {
+  type    = string
+  default = "gen2_sa@mytesting-400910.iam.gserviceaccount.com"
+
+}
